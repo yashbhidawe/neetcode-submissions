b04b83cd40d256776c9ff859c86c607038e3fb7d
Yash! <ybh5179@gmail.com>
2026-04-29 21:23:03 +0530

    Add: range-sum-query-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-0.py
@@ -0,0 +1,10 @@
+class NumArray:
+
+    def __init__(self, nums):
+        self.prefix = [0] * (len(nums) + 1)
+
+        for i in range(len(nums)):
+            self.prefix[i + 1] = self.prefix[i] + nums[i]
+
+    def sumRange(self, left, right):
+        return self.prefix[right + 1] - self.prefix[left]
