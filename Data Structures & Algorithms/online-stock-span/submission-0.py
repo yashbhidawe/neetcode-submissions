799a922aa194ef62661ee798231b9a447beabd98
Yash! <ybh5179@gmail.com>
2026-07-31 20:46:51 +0530

    Add: online-stock-span - submission-0

---
+++ Data Structures & Algorithms/online-stock-span/submission-0.py
@@ -0,0 +1,11 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.arr = []
+
+    def next(self, price: int) -> int:
+        self.arr.append(price)
+        i = len(self.arr) - 2
+        while i >= 0 and self.arr[i] <= price:
+            i -= 1
+        return len(self.arr) - i - 1
